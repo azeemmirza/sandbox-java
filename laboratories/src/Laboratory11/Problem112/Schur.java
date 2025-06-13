dfdaac188d1fb3865afa24aea3b4fb1acaabe362
Helena Pedro <mbeua94@gmail.com>
2025-06-13 14:37:09 -0500

    Problem 11-2 updated - Schur class

---
+++ laboratories/src/Laboratory11/Problem112/Schur.java
@@ -0,0 +1,33 @@
+package Laboratory11.Problem112;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Schur {
+    public boolean checkForSum(List<Integer> list, Integer z) {
+        HashMap<Integer, Integer> h = new HashMap<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            h.put(list.get(i), i);
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            int diff = z - list.get(i);
+
+            if (h.containsKey(diff) && h.get(diff) != i) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Schur schur = new Schur();
+        List<Integer> list = List.of(1, 2, 3, 4, 5);
+        Integer z = 7;
+
+        boolean result = schur.checkForSum(list, z);
+        System.out.println("Is there two elements x, y in the list whose sum is " + z + "? " + result);
+    }
+}
