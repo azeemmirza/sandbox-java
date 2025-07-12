588ef11274b07e84e8a701c02ca1fe1a381d4a2f
Azeem Mirza <azeem.mirza94@gmail.com>
2025-07-12 12:30:00 -0500

    answers pushed

---
+++ CS401/final-exam/src/question_07/PropertyManagementLambdas.java
@@ -0,0 +1,34 @@
+package question_07;
+
+import java.util.function.*;
+import java.util.*;
+
+public class PropertyManagementLambdas {
+
+    public static void main(String[] args) {
+        // A. Address Standardization
+        Function<String, String> standardizeAddress = address -> address.toUpperCase();
+
+        // B. Rent Calculation
+        BiFunction<Double, Double, Double> calculateTotalRent = (baseRent, fees) -> baseRent + fees;
+
+        // C. Property Sorting
+        Comparator<Property> sortByBedroomsDescending = (p1, p2) -> Integer.compare(p2.getBedrooms(), p1.getBedrooms());
+
+        // D. Owner Display Name
+        BiFunction<String, String, String> displayOwnerName = (first, last) -> first + " " + last;
+
+        // E. Maintenance Request ID Generation
+        BiFunction<String, Integer, String> generateRequestId = (propertyId, requestNo) -> propertyId + "-" + requestNo;
+    }
+
+    static class Property {
+        private int bedrooms;
+        public Property(int bedrooms) {
+            this.bedrooms = bedrooms;
+        }
+        public int getBedrooms() {
+            return bedrooms;
+        }
+    }
+}
