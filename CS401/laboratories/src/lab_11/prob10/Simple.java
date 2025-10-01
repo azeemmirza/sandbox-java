b61f3633ddc4b6342298204636fc889b96f7b885
Azeem Mirza <azeem.mirza94@gmail.com>
2025-10-01 10:48:23 -0500

    update sandbox java

--- CS401/laboratories/src/lab_11/prob10/Simple.java
+++
@@ -1,9 +0,0 @@
-package lab_11.prob10;
-
-public class Simple {
-    boolean flag = false;
-
-    Simple(boolean f) {
-        flag = f;
-    }
-}
--- CS401/laboratories/src/lab_11/prob11b/TriFunction.java
+++
@@ -1,6 +0,0 @@
-package lab_11.prob11b;
-
-@FunctionalInterface
-public interface TriFunction<S, T, U, R> {
-    R apply(S s, T t, U u);
-}
